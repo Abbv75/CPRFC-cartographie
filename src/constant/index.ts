3925b0f667e81beb355e5d5a3abcec62a51549e8
abbv75 <bore.younous59@gmail.com>
2025-12-18 12:58:11 +0000

    Refactor: Streamline environment variable configuration
    Remove `_DEV` and `_PROD` suffixes from environment variables, consolidating them into single variables (e.g., `REACT_APP_API_URL`). Eliminate `NODE_ENV` conditional checks in `axiosInstance.ts`, `icon.ts`, and `index.ts`. This simplifies code and allows for environment-specific `.env` files to manage values.

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -8,10 +8,6 @@ export { SIDE_PANEL_LIST } from "./sidePanelList"
 export { CARTO_MENU_EN_TETE_ZONE } from "./cartoMenuEntete";
 export { default as ICON } from './icon';
 
-export const REACT_APP_SHAPE_FILE_URL = process.env.NODE_ENV == "production"
-    ? process.env.REACT_APP_SHAPE_FILE_URL_PROD
-    : process.env.REACT_APP_SHAPE_FILE_URL_DEV;
+export const REACT_APP_SHAPE_FILE_URL = process.env.REACT_APP_SHAPE_FILE_URL;
     
-export const REACT_APP_API_URL = process.env.NODE_ENV == "production"
-    ? process.env.REACT_APP_API_URL_PROD
-    : process.env.REACT_APP_API_URL_DEV;
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL
